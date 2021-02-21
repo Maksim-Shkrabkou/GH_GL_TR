82ac9ed3ca7871caf5dd2f46441cf085f215e39a
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-02-20 20:01:18 -0500

    initial commit

---
+++ src/test/java/com/example/test/ExampleTest.java
@@ -0,0 +1,24 @@
+package com.example.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class ExampleTest {
+
+    @Test
+    void testCanSignIn() {
+        Configuration.timeout = 6000;
+        Selenide.open("https://angular.realworld.io/");
+        $(By.xpath("//a[normalize-space()='Sign in']")).click();
+        $(By.xpath("//input[@placeholder='Email']")).setValue("max1995@gmail.com");
+        $(By.xpath("//input[@placeholder='Password']")).setValue("password");
+        $(By.xpath("//button[normalize-space()='Sign in']")).click();
+        $(By.xpath("//li[normalize-space()='email or password is invalid']"))
+                .shouldHave(Condition.exactText("email or password is invalid"));
+    }
+}
