915b01308cc3ae325bc0a382858ebd7df0a169fa
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-02-22 21:18:40 -0500

    Chrome options was added

--- src/test/java/com/example/test/ExampleTest.java
+++ src/test/java/com/example/test/ExampleTest.java
@@ -13,8 +13,9 @@ public class ExampleTest {
 
     @BeforeAll
     static void setUp() {
-        Configuration.headless = true;
+        // Configuration.headless = true;
         Configuration.timeout = 6000;
+        Configuration.browser = "com.example.test.ChromeWebdriverProvider";
         // Configuration.remote = "http://localhost:4444/wd/hub";
     }
 
